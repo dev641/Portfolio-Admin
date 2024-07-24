49a5c9489f738b980fb0645510174dc2078edfbe
Devashish <dev641science@gmail.com>
2024-07-24 17:26:35 +0530

    Eslint and prettier  added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,47 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    "react",
+    "react-refresh",
+    "react-hooks",
+    "@typescript-eslint",
+    "jsx-a11y",
+    "import",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off", // Not required with React 17+
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
